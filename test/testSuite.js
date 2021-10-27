b2526e1a0810e565c737a2e53032d2ba5e44f2ab
castzad <castzad@gmail.com>
2021-10-27 12:17:32 -0700

    Initial quick sort test completed

--- test/testSuite.js
+++ test/testSuite.js
@@ -1,7 +1,9 @@
 var assert = require('assert')
 findMedianSortedArrays = require('../medianOfTwoSortedArrays/medianOfTwoSortedArrays.js');
 twoSum = require('../2sum/2sum.js');
+quickSort = require('../quickSort/quickSort.js');
 
+// Find Median Sorted Arrays Tests
 describe('findMedianSortedArrays', function() {
   it('Should find median of two sorted arrays', function() {
     assert.equal(findMedianSortedArrays([1, 2], [3]), 2)
@@ -17,8 +19,16 @@ describe('findMedianSortedArrays', function() {
   })
 })
 
+// 2sum Tests
 describe('2sum', function() {
   it('Should find indices of an array of nums', function() {
     assert.equal(JSON.stringify(twoSum([2, 7, 11, 15], 9)), JSON.stringify([0, 1]))
+  })
+})
+
+// Quick Sort Tests
+describe('quickSort', function () {
+  it('Should sort an array using the quick sort algorithm', function () {
+    assert.equal([quickSort([3, 1]), [1, 3]])
   })
 })
